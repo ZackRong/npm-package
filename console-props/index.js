2ceb7628ac677a2d7c03bcbbc9ca1bd155250be4
rongzhenxin <zx.rong@zuolin.com>
2017-12-20 17:49:14 +0800

    add

---
+++ console-props/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by rongzhx on 2017/12/20 0020.
+ */
+module.exports = require('./lib');
+exports.default = require('./lib');
---
+++ console-props/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by rongzhx on 2017/12/20 0020.
+ */
+import React, { PureComponent, Component } from 'react';
+
+class ConsoleProps extends (PureComponent || Component){
+	render = () => {
+		const { text } = this.props;
+		return (
+			<div>
+			{ `hello ${text} !!` }
+			</div>
+		)
+	}
+}
+
+export default ConsoleProps;
